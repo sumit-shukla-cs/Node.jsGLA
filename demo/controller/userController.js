cb874b78b781e1dd2eb66e3fb74be55be6bc0c8e
sumit-shukla-cs <sumitshukla0067@gmail.com>
2026-03-17 15:40:53 +0530

    express

--- demo/controller/userController.js
+++ demo/controller/userController.js
@@ -16,22 +16,38 @@ exports.getUserById = (req, res) => {
 
     res.json(user);
 }
-exports.getUserById = (req, res) => {
-    const userId = req.params.id;
-    const name = req.query.name;
-    const header = req.headers;
-    console.log("header: ", header);
-    console.log("name: ", name);
-    console.log("userId: ", userId);
 
-    let user={};
-    user.map((objectFromList)=>{
-        console.log("objectFromList: ", objectFromList);
-        const id=objectFromListq.id;
-        if(id==userId){
-            user=objectFromList;
-        }
-    })
-
-    res.json(user);
+// GET /get/products
+exports.getProducts = (req, res) => {
+    res.json(products)
 }
+
+// GET /get/products/:id
+exports.getProductById = (req, res) => {
+    const productId = req.params.id;
+    
+    const product = users.find(p => p.id == productId);
+
+    res.json(product);
+}
+
+// POST /add/products
+exports.addProduct = (req, res) => {
+    console.log("res:", req.body)
+    res.json(req.body)
+}
+
+// DELETE /get/products/:id
+exports.deleteProduct = (req, res) => {
+    const productId = req.params.id;
+    
+    const productIndex = users.findIndex(p => p.id == productId);
+    
+    if(productIndex > -1) {
+        const deletedProduct = users.splice(productIndex, 1);
+        res.json({message: "Product deleted", product: deletedProduct[0]});
+    } else {
+        res.status(404).json({message: "Product not found"});
+    }
+}
+
