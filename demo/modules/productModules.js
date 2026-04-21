94909df46eacfed4a910bf88ef5243a7388b9a97
sumit-shukla-cs <sumitshukla0067@gmail.com>
2026-04-21 14:36:00 +0530

    refactor: format product module

--- demo/modules/productModules.js
+++ demo/modules/productModules.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    category: String,
-    description: String,
-    inStock: Boolean,
-    quantity: Number,
+  name: String,
+  price: Number,
+  category: String,
+  description: String,
+  inStock: Boolean,
+  quantity: Number,
 });
 
 // Pre-save hook
-productSchema.pre('save', function (next) {
-    console.log('Saving product...');
-    next();
+productSchema.pre("save", function (next) {
+  console.log("Saving product...");
+  next();
 });
 
 // Post-save hook
-productSchema.post('save', function (doc) {
-    console.log('Product saved:', doc);
+productSchema.post("save", function (doc) {
+  console.log("Product saved:", doc);
 });
 
 const Product = mongoose.model("Product", productSchema, "products");
