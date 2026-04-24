00f48e90fea0011f48239925324908af545f1bd0
sumit-shukla-cs <sumitshukla0067@gmail.com>
2026-04-24 10:45:29 +0530

    userMiddleware

---
+++ demo/middleware/userMiddleware.js
@@ -0,0 +1,18 @@
+const validate = (schema)=>{
+    return (req,res,next)=>{
+        const {error} = schema.validate(req.body);
+        if(error){
+            const errorMessage = error.details.map((err)=>({
+                field: err.path.join(','),
+                message: err.message
+            }));
+            return res.status(400).json({
+                success: false,
+                error: errorMessage
+            });
+        }else{
+            next();
+        }   
+    }                       
+
+            }
